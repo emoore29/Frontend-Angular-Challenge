db6ea01329e612bd9d45f8ab960c8f43a52f9608
Emma Moore <113172968+emoore29@users.noreply.github.com>
2024-02-01 19:59:13 +0800

    added header and navigation components

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Greenbase Application
- 
+
+## Planned components
+
+Main App, including:
+
+- Header (modernise, search bar, chat icon, notification icon, profile)
+- Navigation sidebar (dashboard, orders, other information, etc)
+- Knowledge base:
+  _ Knowledge "cards" (getting started, personal settings)... all the same but with different content
+  _ Community forum, Webinars \* Still need help component
--- greenbase-app/src/app/app.component.ts
+++ greenbase-app/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppHeaderComponent, NavigationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'greenbase-app';
