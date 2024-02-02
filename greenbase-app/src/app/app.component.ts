811443b95a881f4389279a7f1696b2fe2776a368
AthenasCode <emoore5203@gmail.com>
2024-02-02 14:37:09 +0800

    rename app-header folder to match component name

--- greenbase-app/src/app/app.component.ts
+++ greenbase-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './app-header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ContentComponent } from './content/content.component';
 
