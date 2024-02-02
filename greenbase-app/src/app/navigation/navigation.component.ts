f0e6500212f67468447a66c0ade946a915510361
AthenasCode <emoore5203@gmail.com>
2024-02-02 13:11:40 +0800

    added basic styling and items to navigation

--- greenbase-app/src/app/navigation/navigation.component.ts
+++ greenbase-app/src/app/navigation/navigation.component.ts
@@ -1,12 +1,73 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface NavItems {
+  name: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.sass'
+  styleUrl: './navigation.component.sass',
 })
 export class NavigationComponent {
+  navItems: NavItems[] = [
+    {
+      name: 'Dashboard',
+      icon: 'homesvg',
+    },
+    {
+      name: 'Orders',
+      icon: 'listsvg',
+    },
+    {
+      name: 'Products',
+      icon: 'svg',
+    },
+    {
+      name: 'Categories',
+      icon: 'svg',
+    },
+    {
+      name: 'Customers',
+      icon: 'svg',
+    },
+    {
+      name: 'Reports',
+      icon: 'svg',
+    },
+    {
+      name: 'Coupons',
+      icon: 'svg',
+    },
+    {
+      name: 'Inbox',
+      icon: 'svg',
+    },
+  ];
 
+  otherInformation: NavItems[] = [
+    {
+      name: 'Knowledge Base',
+      icon: 'svg',
+    },
+    {
+      name: 'Product Updates',
+      icon: 'svg',
+    },
+  ];
+
+  settings: NavItems[] = [
+    {
+      name: 'Personal Settings',
+      icon: 'svg',
+    },
+    {
+      name: 'Global settings',
+      icon: 'svg',
+    },
+  ];
 }
